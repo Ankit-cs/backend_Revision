f21b2527413445d91e1ed56636a2d4509179c9c6
Ankit-cs <ankitcareer018@gmail.com>
2025-04-30 15:04:57 +0530

    just learn how to host nodejs server

--- 03_module/app.js
+++ 03_module/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000 ;
 const db = require("./models/db.model");
 const personRoutes = require('./routers/person.router');
 const menuRoutes = require('./routers/meenu.router');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- 03_module/models/db.model.js
+++ 03_module/models/db.model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/hotels', {
-   
-}).then(() => {
-    console.log('Database connected');
-}).catch((err) => {
-    console.error('Database connection error:', err);
-});
+require('dotenv').config();
+const MongoURL=process.env.DB_URL
+mongoose.connect(MongoURL)
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch((err) => {
+        console.error('Database connection error:', err);
+    });
 
 module.exports = mongoose;
